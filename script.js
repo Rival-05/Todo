4638a40bd72483d129ef9f4a143c3fb0cee57107
Rajat Tripathi <rivalo3.chat@gmail.com>
2024-12-25 23:30:11 +0530

    Modified adding task method.

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 const InputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask() {
+    if(InputBox.value === ''){
+        alert("Doing something is better than doing nothing!");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = InputBox.value;
+        listContainer.appendChild(li);
+    }
+
+}
