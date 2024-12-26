e6f210cb1f27ea91b80dbeeb2fa75642602bae86
Rajat Tripathi <rivalo3.chat@gmail.com>
2024-12-26 16:12:46 +0530

    Done with the Todo List.

--- script.js
+++ script.js
@@ -9,6 +9,36 @@ function addTask() {
         let li = document.createElement("li");
         li.innerHTML = InputBox.value;
         listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "Remove";
+        li.appendChild(span);
     }
-
+    InputBox.value = '';
+    saveData();
 }
+
+listContainer.addEventListener("click",function(plug){
+    if(plug.target.tagName === "LI"){
+        plug.target.classList.toggle("checked");
+        saveData(); 
+    }
+    else if(plug.target.tagName === "SPAN"){
+        plug.target.parentElement.remove();
+        saveData();
+    }
+},false);
+
+InputBox.addEventListener("keydown", function(event){
+    if(event.key === "Enter"){
+        addTask();
+    }
+});
+
+function saveData(){
+    localStorage.setItem("data",listContainer.innerHTML);
+}
+
+function displayData(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+displayData();
